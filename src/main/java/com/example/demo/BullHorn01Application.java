8553309baa3b73d727f9d1ec425b5c304e728b55
GBTC441004UR <GBTC441004UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-03-16 16:20:00 -0400

    Initial commit

---
+++ src/main/java/com/example/demo/BullHorn01Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BullHorn01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BullHorn01Application.class, args);
+	}
+}
---
+++ src/main/java/com/example/demo/model/Message.java
@@ -0,0 +1,59 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Message {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=1, max=140)
+    private String content;
+
+    @NotNull
+    @Size(min=1)
+    private String postedDate;
+
+    @NotNull
+    @Size(min=1)
+    private String sentBy;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getPostedDate() {
+        return postedDate;
+    }
+
+    public void setPostedDate(String postedDate) {
+        this.postedDate = postedDate;
+    }
+
+    public String getSentBy() {
+        return sentBy;
+    }
+
+    public void setSentBy(String sentBy) {
+        this.sentBy = sentBy;
+    }
+}
